1a4ae5ca545837c6c70ad1728cf8c0c79fa486e8
sriulfaberliani <sriulfaberliani2002@gmail.com>
2022-06-09 10:56:43 +0700

    peminjaman

--- app/Models/M_Peminjaman.php
+++ app/Models/M_Peminjaman.php
@@ -13,7 +13,11 @@ class M_Peminjaman extends Model
 
     public function getAllData()
     {
-        return $this->db->table('perpussmkn2.tabel_peminjaman, perpussmkn2.tabel_detail_peminjaman, perpussmkn2.tabel_anggota,perpussmkn2.tabel_user, perpussmkn2.tabel_buku ')->get()->getResultArray();
+        return $this->db->table('perpussmkn2.tabel_peminjaman')->get()->getResultArray();
     }
 
+    public function tambah($data)
+    {
+        return $this->db->table('perpussmkn2.tabel_peminjaman')->insert($data);
+    }
 }
