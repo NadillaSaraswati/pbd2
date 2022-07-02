56b64b365faee3bc660916539ccca65442bdf535
sriulfaberliani <sriulfaberliani2002@gmail.com>
2022-07-02 22:31:32 +0700

    script

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,6 +6,29 @@ $(document).on('click', '#btn-edit', function () {
     $('.modal-body #no_induk').val($(this).data('no_induk'));
 })
 
+$(document).on('click', '#btn-edit', function () {
+    $('.modal-body #id_peminjaman').val($(this).data('id_peminjaman'));
+    $('.modal-body #id_anggota').val($(this).data('id_anggota'));
+    $('.modal-body #id_buku').val($(this).data('id_buku'));
+    $('.modal-body #id_user').val($(this).data('id_user'));
+})
+
 $(document).on('click', '#btn-hapus', function () {
     $('.modal-body #id_anggota').val($(this).data('id_anggota'));
+  })
+
+  $(document).on('click', '#btn-hapus', function () {
+    $('.modal-body #id_peminjaman').val($(this).data('id_peminjaman'));
+  })
+
+  $(document).on('click', '#btn-edit', function () {
+    $('.modal-body #id_pengembalian').val($(this).data('id_pengembalian'));
+    $('.modal-body #id_anggota').val($(this).data('id_anggota'));
+    $('.modal-body #id_user').val($(this).data('id_user'));
+    $('.modal-body #id_pengembalian').val($(this).data('id_pengembalian'));
+    
+})
+
+$(document).on('click', '#btn-hapus', function () {
+    $('.modal-body #id_pengembalian').val($(this).data('id_pengembalian'));
   })
